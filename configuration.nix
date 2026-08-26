e9047e6f82163ac0f1b3c784c0b0eff6bb68b179
Gorciu <gorciuyt@gmail.com>
2026-08-26 18:31:29 +0200

    feat: add different power button handling

--- configuration.nix
+++ configuration.nix
@@ -9,6 +9,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.initrd.systemd.enable = true;
+  services.logind.settings.Login.HandlePowerKey = "suspend";
 
   networking.hostName = "Gorciu-Computer";
 
