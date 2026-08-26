1c1ea435a976f280f7f026dd666486ec735070c6
Gorciu <gorciuyt@gmail.com>
2026-08-26 10:07:36 +0200

    refactor: organize some shit

--- configuration.nix
+++ configuration.nix
@@ -1,34 +1,21 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
       ./hardware-configuration.nix
     ];
 
-  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.initrd.systemd.enable = true;
 
-  networking.hostName = "Gorciu-Computer"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.hostName = "Gorciu-Computer";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
   time.timeZone = "Europe/Warsaw";
 
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
@@ -43,16 +30,13 @@
     LC_TIME = "pl_PL.UTF-8";
   };
 
-  # Configure keymap in X11
   services.xserver.xkb = {
     layout = "pl";
     variant = "";
   };
 
-  # Configure console keymap
   console.keyMap = "pl2";
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users."gorciu" = {
     isNormalUser = true;
     description = "Gorciu";
@@ -63,30 +47,43 @@
     packages = with pkgs; [];
   };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
+     # code editors 
      neovim
+
+     # applications 
+     firefox 
+     satty
      kitty
-     fastfetch
-     firefox
      vesktop
+
+     # wm look and feel thingies
+     waybar
      rofi
      awww
+     mako
      kdePackages.breeze
-     waybar
-     git
+     hyprshot
+     hypridle
+     hyprlock
+
+     # command line tools 
+     fastfetch
      wl-clipboard
      cliphist
-     hypridle
-     mako
      unzip
+     git
+     tmux
+
+     # coding enviorments (i can't spell this word ok????)
      go
      nodejs_26
      deno
-     hyprshot
-     satty
-     tmux
+     qemu_full
+     gcc
+     gnumake
+     xorriso
+     nasm
   ];
 
   programs.hyprland = {
@@ -132,31 +129,6 @@
     };
   };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "26.05"; # Did you read the comment?
-
+  # first version of NixOS on the system  
+  system.stateVersion = "26.05"; 
 }
